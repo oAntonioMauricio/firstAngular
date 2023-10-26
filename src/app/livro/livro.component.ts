67d6f2b07f2892386d14b8228bbd05ed588331d6
António Maurício <mauriciooqq@gmail.com>
2023-10-26 12:58:30 +0100

    update livro comp

--- src/app/livro/livro.component.ts
+++ src/app/livro/livro.component.ts
@@ -1,5 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {faEye} from '@fortawesome/free-solid-svg-icons';
+
+interface Livro {
+  titulo: string,
+  autor: string,
+  ano_publicacao: number,
+  imagem: string
+}
 
 @Component({
   selector: 'app-livro',
@@ -8,4 +15,20 @@ import {faEye} from '@fortawesome/free-solid-svg-icons';
 })
 export class LivroComponent {
   faEye = faEye;
+
+  @Input() nome!: string;
+  @Input() autor!: string;
+  @Input() ano_publicacao!: number;
+  @Input() imagem!: string;
+
+  // livro: Livro;
+
+  constructor() {
+    // this.livro = {
+    //   titulo: "Memorial do Convento",
+    //   autor: "José Saramago",
+    //   ano_publicacao: 1982,
+    //   imagem: "assets/livraria/capas/12.jpg"
+    // }
+  }
 }
