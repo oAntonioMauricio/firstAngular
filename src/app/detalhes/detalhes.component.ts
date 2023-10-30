8ee1d94f1dafdaa7b363a0c5fc3ad86cb4dcf44e
António <oantoniomauricio@gmail.com>
2023-10-30 19:15:56 +0000

    add service to store favs at localStorage

--- src/app/detalhes/detalhes.component.ts
+++ src/app/detalhes/detalhes.component.ts
@@ -3,32 +3,39 @@ import {ActivatedRoute} from "@angular/router";
 import detalhes, {DetalheLivro} from "src/data/dados_livraria/detalhes_livros";
 import {faBookmark} from '@fortawesome/free-regular-svg-icons';
 import {faChevronDown, faBookmark as faBookmarkSolid} from '@fortawesome/free-solid-svg-icons';
+import {LivrosService} from "../services/livros.service";
 
 @Component({
-    selector: 'app-detalhes',
-    templateUrl: './detalhes.component.html',
-    styleUrls: ['./detalhes.component.scss']
+  selector: 'app-detalhes',
+  templateUrl: './detalhes.component.html',
+  styleUrls: ['./detalhes.component.scss']
 })
 export class DetalhesComponent {
 
-    // icons
-    faBookmark = faBookmark;
-    faBookmarkSolid = faBookmarkSolid;
-    faChevronDown = faChevronDown;
+  // icons
+  faBookmark = faBookmark;
+  faBookmarkSolid = faBookmarkSolid;
+  faChevronDown = faChevronDown;
 
-    // attributes
-    id_livro: number;
-    DetalheLivro: DetalheLivro;
-    iconToUse = faBookmark;
+  // attributes
+  id_livro: number;
+  DetalheLivro: DetalheLivro;
 
-    constructor(private route: ActivatedRoute) {
-        this.id_livro = route.snapshot.params['id_livro'];
-        this.DetalheLivro = detalhes[this.id_livro];
-    }
+  constructor(private route: ActivatedRoute, private livrosService: LivrosService) {
+    this.id_livro = route.snapshot.params['id_livro'];
+    this.DetalheLivro = detalhes[this.id_livro];
+  }
 
-    // methods
-    changeIcon = () => {
-        this.iconToUse === this.faBookmark ? this.iconToUse = this.faBookmarkSolid : this.iconToUse = this.faBookmark;
-    }
+  // methods
+  toogleFav() {
+    // change state
+    this.livrosService.toogleFav(this.id_livro);
+
+    console.log("changed!");
+  }
+
+  checkIfFav() {
+    return this.livrosService.checkIfFav(this.id_livro);
+  }
 
 }
--- src/app/services/livros.service.ts
+++ src/app/services/livros.service.ts
@@ -1,8 +1,38 @@
 import {Injectable} from "@angular/core";
 
-@Injectable(({
-    providedIn: "root"
-}))
+@Injectable({
+  providedIn: "root"
+})
+
 export class LivrosService {
-    bookmarks = {}
+
+  bookmarks: { [key: number]: boolean } = {};
+
+  toogleFav(id_livro: number): void {
+    // change
+    this.bookmarks[id_livro] = !this.bookmarks[id_livro];
+
+    // send to local storage
+    localStorage.setItem("bookmarks", JSON.stringify(this.bookmarks));
+
+    console.log(this.bookmarks)
+    console.log(localStorage.getItem("bookmarks"))
+  }
+
+  checkIfFav(id_livro: number): boolean {
+    return this.bookmarks[id_livro];
+  }
+
+  constructor() {
+    // deal with local storage
+    let bookmarksItem = localStorage.getItem("bookmarks");
+
+    // check if it's non-existent
+    if (!bookmarksItem) {
+      localStorage.setItem("bookmarks", "{}");
+      bookmarksItem = "{}";
+    }
+
+    this.bookmarks = JSON.parse(bookmarksItem);
+  }
 }
